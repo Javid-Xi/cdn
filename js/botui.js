3744904904d88fe705084471f4b2fbe21f88e7fc
Javid-Xi <jiaweixi1012@163.com>
2020-04-15 19:07:21 +0800

    Update botui.js

--- js/botui.js
+++ js/botui.js
@@ -66,7 +66,7 @@ function bot_ui_ini() {
         end = function () {
             botui.message.add({
                 delay: 1500,
-                content: "![...](https://cdn.jsdelivr.net/gh/Javid-Xi/cdn@1.4.1/img/material/rp-dora1.jpg)"
+                content: "![...](https://cdn.jsdelivr.net/gh/Javid-Xi/cdn@1.4.2/img/material/rp-dora1.jpg)"
             })
         },
         secondpart = function () {
@@ -268,7 +268,7 @@ function bot_ui_ini() {
         end1 = function () {
             botui.message.add({
                 delay: 1500,
-                content: "![...](https://cdn.jsdelivr.net/gh/Javid-Xi/cdn@2.2.0/img/material/rp-dora1.jpg)"
+                content: "![...](https://cdn.jsdelivr.net/gh/Javid-Xi/cdn@1.4.2/img/material/rp-dora2.jpg)"
             })
         },
         fifthpart = function () {
